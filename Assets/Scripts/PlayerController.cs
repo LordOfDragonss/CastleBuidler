03f1dcd478fa83a934cfd7a460a067c939a53afc
Mike Tool <miketooltje@gmail.com>
2025-03-26 16:53:17 +0100

    Ground checking

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,27 +7,38 @@ public class PlayerController : MonoBehaviour
 {
     public float Movementspeed;
     public float JumpForce;
+    public float groundCheckDistance = 0.5f;
     Rigidbody2D rb;
     Vector3 moveinput;
+    public LayerMask floorLayers;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        rb = GetComponentInChildren<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     private void Update()
     {
-        rb.linearVelocity = (new Vector2( moveinput.x * Movementspeed,rb.linearVelocity.y));
+        rb.linearVelocity = (new Vector2(moveinput.x * Movementspeed, rb.linearVelocity.y));
     }
 
     public void OnMove(InputValue value)
     {
         moveinput = value.Get<Vector2>();
-
     }
 
     public void OnJump()
     {
+        if (isOnGround())
+        {
         rb.linearVelocity = new Vector2(rb.linearVelocity.x, JumpForce);
+        }
+    }
+
+    public bool isOnGround()
+    {
+        return Physics2D.Raycast(transform.position, Vector2.down, groundCheckDistance, floorLayers) ||
+               Physics2D.Raycast(transform.position + new Vector3(0.5f, 0, 0), Vector2.down, groundCheckDistance, floorLayers) ||
+               Physics2D.Raycast(transform.position + new Vector3(-0.5f, 0, 0), Vector2.down, groundCheckDistance, floorLayers);
     }
 }
